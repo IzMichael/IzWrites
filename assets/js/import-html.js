96455f3f3687516a36e5f3e1d41e3761ad3ddee7
IzMichael <themichael77@outlook.com>
2020-09-16 16:00:13 +1200

    Initial Build and 1st Post

--- assets/js/import-html.js
+++ assets/js/import-html.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    $('.headerimport').load("/universal-assets/header.html");
-  });
+$(document).ready(function () {
+  $('.headerimport').load("/universal-assets/header.html");
+});
 
-$(document).ready(function(){
-    $('.footerimport').load("/universal-assets/footer.html");
-  });
+$(document).ready(function () {
+  $('.footerimport').load("/universal-assets/footer.html");
+});
 
-$(document).ready(function(){
-    $('.menuimport').load("/universal-assets/menu.html");
-  });
+$(document).ready(function () {
+  $('.menuimport').load("/universal-assets/menu.html");
+});
+
+$(document).ready(function () {
+  $('.postlistimport').load("/universal-assets/posts.html");
+});
